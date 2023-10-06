b5b50d9db7693794b4e7da98d7a0378de11b9ad4
Jason Chan <jasonchan@192.168.1.103>
2023-10-06 01:24:31 +0100

    finished most of the weather data page

--- weather/static/weather/index.js
+++ weather/static/weather/index.js
@@ -10,6 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
     })
 })
 
+
 //clearing all divs
 function clear_page() {
     document.querySelectorAll("#search-page, #city-weather-page, #city-selection-page").forEach(div => {
@@ -98,12 +99,12 @@ function weather_data(city_location) {
     
 };
 
+//loading basic weather page
 function load_weather_page(city_general_info, weather_info) {
     console.log(city_general_info, weather_info);
     clear_page()
     const weather_page = document.querySelector("#city-weather-page")
     weather_page.style.display = "block";
-    const line_break = document.createElement("br")
 
     //title for page with city
     const city_title_div = document.createElement("div")
@@ -119,36 +120,114 @@ function load_weather_page(city_general_info, weather_info) {
 
     //adding the date buttons 
     const date_div = weather_page.querySelector("#city-weather-page-button-div");
-    for (date in weather_info.daily.time) {
+
+    for (let i=0;i<weather_info.daily.time.length;i++) {
         let date_button = document.createElement("button");
-        date_button.innerHTML = weather_info.daily.time[date]
-        date_button.setAttribute("class", "weather-page-button")
+        date_button.innerHTML = weather_info.daily.time[i]
+        if (i===0){
+            date_button.setAttribute("class","button-active");
+        } else {
+            date_button.setAttribute("class", "button-inactive");
+        }
+        date_button.addEventListener("click", ()=>{
+            load_other_info(i,weather_info)
+            document.querySelectorAll("#city-weather-page #city-weather-page-button-div button").forEach((element)=>{
+                element.className = "button-inactive"
+            })
+            date_button.className="button-active"
+        });
         date_div.append(date_button)
     }
 
-    //adding the daily divs
-    const daily_div = weather_page.querySelector("#city-weather-page-daily-div");
-
     //finding current time and date and current temp
-    const current_temp_div = document.createElement("div");
     const currentDate_overall = new Date().toISOString();
     const currentTime = currentDate_overall.substring(11,13);
     const currentTime_int = parseInt(currentTime)
     const currentDate = currentDate_overall.substring(0,10);
+    console.log(`TIME:${currentDate_overall}time:${currentTime_int}`)
     const current_temp =  weather_info.hourly.temperature_2m[currentTime_int]
 
     //adding current temp to its div
-    current_temp_div.setAttribute("class", "weather-page-current-temp-div")
-    const current_temp_note_div = document.createElement("span")
-    current_temp_note_div.innerHTML = "The current temperature is:"
-    const current_temp_int_div = document.createElement("b")
-    current_temp_int_div.innerHTML = `${current_temp}`
+    const daily_temp_div = document.querySelector("#city-weather-page #city-weather-page-daily-div .weather-page-current-temp-div ");
+    daily_temp_div.querySelector("#city-weather-page-daily-temp-text").innerHTML = "The current temperature is:"
+    daily_temp_div.querySelector("b").innerHTML = `${current_temp}${weather_info.daily_units.temperature_2m_max}`;
 
     //adding todays date
-    const todays_date = document.createElement("span")
-    todays_date.innerHTML = `Todays date is: ${currentDate}`
+    daily_temp_div.querySelector("#city-weather-page-daily-temp-date").innerHTML = `Todays date is: ${currentDate}`
 
-    current_temp_div.append(todays_date, line_break,current_temp_note_div,line_break,current_temp_int_div)
+    //daily info div
+    load_other_info(0,weather_info)
 
-    daily_div.append(current_temp_div)
+}
+
+//loading the daily info and the hourly info
+function load_other_info(date, weather_info) {
+    console.log(date,weather_info)
+
+
+    //filling out the daily info div
+    const daily_info_div = document.querySelector("#city-weather-page #city-weather-page-daily-div .city-weather-page-daily-info-div");
+    daily_info_div.querySelector("#sunrise b").innerHTML = `${weather_info.daily.sunrise[date].substring(11,16)}`
+    daily_info_div.querySelector("#sunset b").innerHTML = `${weather_info.daily.sunset[date].substring(11,16)}`
+    daily_info_div.querySelector("#maxTemperature b").innerHTML = `${weather_info.daily.temperature_2m_max[date]}${weather_info.daily_units.temperature_2m_max}`
+    daily_info_div.querySelector("#minTemperature b").innerHTML = `${weather_info.daily.temperature_2m_min[date]} ${weather_info.daily_units.temperature_2m_max}`
+    daily_info_div.querySelector("#uvIndex b").innerHTML = `${weather_info.daily.uv_index_max[date]}`
+    daily_info_div.querySelector("#precipitationSum b").innerHTML = `${weather_info.daily.precipitation_sum[date]} ${weather_info.daily_units.precipitation_sum}`
+
+    //filling out hourly div
+    const hourly_table = document.querySelector("#city-weather-page #city-weather-page-hourly-div #hourly-info-table");
+    const time_row = hourly_table.querySelector("#time")
+    time_row.innerHTML = "";
+
+    const start_point = date*24;
+    const end_point = start_point+24;
+
+    //adding function to buttons 
+    const hourly_div = document.querySelector("#city-weather-page #city-weather-page-hourly-div");
+    hourly_div.querySelector("#temperature-button").addEventListener("click", ()=>{change_hourly_div_display("temperature",weather_info.hourly.temperature_2m.slice(start_point,end_point))});
+    hourly_div.querySelector("#humidity-button").addEventListener("click", ()=>{change_hourly_div_display("humidity",weather_info.hourly.relativehumidity_2m.slice(start_point,end_point))});
+    hourly_div.querySelector("#precipitation-sum-button").addEventListener("click", ()=>{change_hourly_div_display("precipitationSum",weather_info.hourly.precipitation.slice(start_point,end_point))});
+
+    for (let i=start_point;i<end_point;i++) {
+        //inserting time data
+        let time_data = document.createElement("td");
+        time_data.innerHTML = `${weather_info.hourly.time[i].substring(11,17)}`;
+        time_row.append(time_data);
+    };
+    change_hourly_div_display("temperature",weather_info.hourly.temperature_2m.slice(start_point,end_point));
+}
+
+
+//adding functinoality to the hourly div
+function change_hourly_div_display (title, data){
+    const data_row = document.querySelector("#city-weather-page #city-weather-page-hourly-div .row .col-9 #data-points")
+    data_row.innerHTML = "";
+
+    //changing button classes to set active one
+    const hourly_div = document.querySelector("#city-weather-page #city-weather-page-hourly-div");
+    hourly_div.querySelectorAll("#temperature-button, #humidity-button, #precipitation-sum-button").forEach((element)=>{
+        element.className = "button-inactive"
+    })
+    
+    //inserting data into the table
+    for (let i=0;i<data.length;i++){
+        let data_point = document.createElement("td");
+        data_point.innerHTML = `${data[i]}`
+        data_row.append(data_point);
+    }
+
+    //writing the header
+    const header = document.querySelector("#city-weather-page #city-weather-page-hourly-div .row .col-3 #data-header th");
+    header.innerHTML = "";
+    if (title === "temperature"){
+        header.innerHTML = "Temperature(°C):";
+        hourly_div.querySelector("#temperature-button").className = "button-active";
+
+    } else if (title === "humidity"){
+        header.innerHTML = "Humidity(%):";
+        hourly_div.querySelector("#humidity-button").className = "button-active";
+    } else if (title === "precipitationSum"){
+        header.innerHTML = "Precipitation Sum(mm):"
+        hourly_div.querySelector("#precipitation-sum-button").className = "button-active";
+    }
 }
