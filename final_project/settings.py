947a47442287c235ed5b5c2d23a1976b69c06473
Jason Chan <jasonchan@192.168.1.103>
2023-10-07 01:11:14 +0100

    added registration and login pages

--- final_project/settings.py
+++ final_project/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     }
 }
 
-
+AUTH_USER_MODEL = 'weather.User'
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
--- weather/views.py
+++ weather/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db import IntegrityError
+
+
+from .models import User
 
 # Create your views here.
 
 def index(request):
     return render(request, "weather/index.html")
+
+def register(request):
+    if request.method == "GET":
+        return render(request, "weather/register.html")
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        confirmation = request.POST["password-confirmation"]
+        if password != confirmation:
+            return render(request, "weather/register.html", {"message": "Passwords do not match"})
+        try:
+            user = User.objects.create_user(username, email = None, password = confirmation)
+            user.save()
+        except IntegrityError:
+            return render(request,"weather/register.html", {"message": "Username is already in use"}) 
+
+        return render(request, 'weather/login.html')
+
+def login(request):
+    return render(request, "weather/login.html")
